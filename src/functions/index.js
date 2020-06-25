11751158f2f0dfc210892afb0dd199d8e5298fe5
leeszeray <leeszeray@initiare.lan>
2020-06-25 15:21:06 +0800

    firebase cloud functions for support

--- src/functions/index.js
+++ src/functions/index.js
@@ -32,13 +32,13 @@ exports.GetAllFAQ = functions.https.onRequest(async (req, res) => {
     });
 });
 
+// create an FAQ Document
 exports.CreateFAQDocument = functions.https.onRequest(async (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "POST");
   res.set("Access-Control-Allow-Headers", "Content-Type");
   if (req.method !== "OPTIONS" && req.method !== "POST")
     return res.status(404).send("Method not supported");
-
 
   var writeResult = await admin.firestore().collection("faq").add({
     question: `${req.body.question}`,
@@ -49,6 +49,7 @@ exports.CreateFAQDocument = functions.https.onRequest(async (req, res) => {
   res.json(writeResult.id);
 });
 
+// update existing faq document
 exports.UpdateFAQDocument = functions.https.onRequest(async (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "PUT");
@@ -65,6 +66,7 @@ exports.UpdateFAQDocument = functions.https.onRequest(async (req, res) => {
     res.json({result: req.body.id});
 });
 
+// delete existing faq document
 exports.DeleteFAQDocument = functions.https.onRequest(async (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "DELETE");
@@ -78,81 +80,7 @@ exports.DeleteFAQDocument = functions.https.onRequest(async (req, res) => {
   res.json({ result: req.body.id});
 });
 
-exports.RaiseTicket = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Methods", "GET");
-  res.set("Access-Control-Allow-Headers", "Content-Type");
-  res.set("Access-Control-Max-Age", "3600");
-
-  // Grab the text parameters & form variable and turn it into an array
-  const newDocument = {
-    email: req.query.email,
-    name: req.query.name,
-    category: req.query.category,
-    resolved: false,
-    timestamp: FieldValue.serverTimestamp(),
-  };
-  // Push the new faq into Cloud Firestore using the Firebase Admin SDK.
-  const writeResult = await admin
-    .firestore()
-    .collection("support")
-    .add(newDocument);
-  // Send back a message that we've succesfully written the message
-  res.json({ result: `Raised support ticket of ID: ${writeResult.id}.` });
-});
-
-exports.ReachCustomer = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Methods", "GET");
-  res.set("Access-Control-Allow-Headers", "Content-Type");
-  res.set("Access-Control-Max-Age", "3600");
-
-  // Grab the text parameters & form variable and turn it into an array
-  const newDocument = {
-    email: req.query.email,
-    name: req.query.name,
-    //...
-  };
-  // implement email feature here to reach customer through GUI
-  res.json({ result: "Success!" });
-});
-
-exports.CloseTicket = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Methods", "GET");
-  res.set("Access-Control-Allow-Headers", "Content-Type");
-  res.set("Access-Control-Max-Age", "3600");
-
-  // Grab the text parameters & form variable and turn it into an array
-  const ticketID = req.query.id;
-
-  // get doc reference based on id
-  const documentReference = admin
-    .firestore()
-    .collection("support")
-    .doc(req.query.id);
-
-  // Set and overwrite a faq into Cloud Firestore using the Firebase Admin SDk.
-  const updateResult = documentReference.get().then(function(doc) {
-    if (doc.exists) {
-      console.log(
-        "Function of update document: document ",
-        doc.data(),
-        "exists!"
-      );
-      doc
-        .update({
-          resolved: true,
-        })
-        .then(function() {
-          res.json({
-            result: `Support ticket with ID: ${updateResult.id} resolved.`,
-          });
-        });
-    } else console.log("Function of update document: document does not exist!");
-  });
-});
-
+// get all support tickets
 exports.GetAllTickets = functions.https.onRequest(async (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "GET");
@@ -175,3 +103,75 @@ exports.GetAllTickets = functions.https.onRequest(async (req, res) => {
       res.json("Got error", error);
     });
 });
+
+exports.GetIndividualTicket = functions.https.onRequest(async (req, res) => { 
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Max-Age", "3600");
+
+  var docRef = admin.firestore().collection("support").doc(`${req.query.id}`);
+
+  docRef.get().then(function(doc) {
+      if (doc.exists) {
+          res.json(doc.data());
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+      }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+})
+
+// create a support ticket (by customer)
+exports.RaiseTicket = functions.https.onRequest(async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "POST");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method !== "OPTIONS" && req.method !== "POST")
+    return res.status(404).send("Method not supported");
+
+  var writeResult = await admin.firestore().collection("support").add({
+    email: `${req.body.email}`,
+    name: `${req.body.name}`,
+    category: `${req.body.category}`,
+    resolved: "false",
+    timestamp: FieldValue.serverTimestamp()
+  });
+  res.json(writeResult.id);
+});
+
+// @todo: base method to reach customer
+exports.ReachCustomer = functions.https.onRequest(async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Max-Age", "3600");
+
+  // Grab the text parameters & form variable and turn it into an array
+  const newDocument = {
+    email: `${ req.body.email}` ,
+    name: `${ req.body.name}`,
+    //...
+  };
+  // implement some feature here to reach customer through GUI
+  res.json({ result: "Success!" });
+});
+
+// update document to reflect resolved status
+exports.CloseTicket = functions.https.onRequest(async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "PUT");
+  res.set("Access-Control-Allow-Headers", "Content-Type");  
+  if (req.method !== "OPTIONS" && req.method !== "PUT")
+    return res.status(404).send("Method not supported");
+
+  var documentReference = admin.firestore().collection("faq").doc(`${req.body.id}`).update({
+    resolved: "true",
+    timestamp: FieldValue.serverTimestamp()
+  });
+  
+  res.json({result: req.body.id});
+});
+
